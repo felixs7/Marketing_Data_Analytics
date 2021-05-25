d4bebc00b55e080367bc0865ed03cce6eabf63a3
felixs7 <37911269+felixs7@users.noreply.github.com>
2021-05-25 12:07:54 +0100

    Updated stored procedure

--- SQL_scripts/CleanMarketingTable_stored_proc.sql
+++ SQL_scripts/CleanMarketingTable_stored_proc.sql
@@ -17,12 +17,12 @@ SELECT [ID]
       ,[Teenhome]
       ,CONVERT(DATETIME,[Dt_Customer],23) as Dt_Customer
       ,[Recency]
-      ,[MntWines]
-      ,[MntFruits]
-      ,[MntMeatProducts]
-      ,[MntFishProducts]
-      ,[MntSweetProducts]
-      ,[MntGoldProds]
+      ,CAST([MntWines] AS int) as [MntWines]
+      ,CAST([MntFruits] AS int)  as [MntFruits]
+      ,CAST([MntMeatProducts] AS int)   as [MntMeatProducts]
+      ,CAST([MntFishProducts] AS int)  as [MntFishProducts]
+      ,CAST([MntSweetProducts] AS int)  as [MntSweetProducts]
+      ,CAST([MntGoldProds] AS int)  as [MntGoldProds]
       ,[NumDealsPurchases]
       ,[NumWebPurchases]
       ,[NumCatalogPurchases]
@@ -36,7 +36,42 @@ SELECT [ID]
       ,[Response]
       ,[Complain]
       ,[Country]
+	  , [MntWines] + [MntFruits] + [MntMeatProducts] + [MntFishProducts] + [MntSweetProducts] + [MntGoldProds] as TotalAmountSpent
+	  , [Kidhome] + [Teenhome] as TotalKids
 INTO marketing_data_prepared
 FROM [marketing_data]
 
+-- A birth year less than 1900 is highly unlikely and is probably due to data entry errors
+-- This is replaced with the median birth year
+DECLARE @median_birthyear AS INT;
+SELECT @median_birthyear = 
+(
+ (SELECT MAX([Year_Birth]) FROM
+   (SELECT TOP 50 PERCENT [Year_Birth] FROM marketing_data_prepared ORDER BY [Year_Birth]) AS BottomHalf)
+ +
+ (SELECT MIN([Year_Birth]) FROM
+   (SELECT TOP 50 PERCENT [Year_Birth] FROM marketing_data_prepared ORDER BY [Year_Birth] DESC) AS TopHalf)
+) / 2 
+
+UPDATE marketing_data_prepared
+SET [Year_Birth] = @median_birthyear
+WHERE  [Year_Birth] < 1900
+
+
+-- create a view to analyse bought products by category. Amount spent on each category is currently pivoted.
+-- Unpivot here to simplify subsequent analysis. We need to use dynamic SQL here to drop and create these views.
+
+exec('
+IF OBJECT_ID(''dbo.VAmountSpentByProduct'', ''v'') IS NOT NULL 
+  DROP VIEW dbo.VAmountSpentByProduct;'
+)
+exec('
+CREATE VIEW dbo.VAmountSpentByProduct AS
+SELECT ID, RotatedCol, ValueCol
+FROM (SELECT  ID, MntWines, MntFruits ,MntMeatProducts ,MntFishProducts ,MntSweetProducts ,MntGoldProds
+              FROM [Marketing_Analytics].[dbo].[marketing_data_prepared]) p
+UNPIVOT(
+ValueCol FOR RotatedCol IN (MntWines, MntFruits ,MntMeatProducts ,MntFishProducts ,MntSweetProducts ,MntGoldProds)
+) as unpvt
+')
 END
